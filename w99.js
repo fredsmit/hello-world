c4c8b0c2d399f9960cc3139a517ceaca0ec76184
ALFREDAS SMITA <ALFREDAS@lt.ibm.com>
2022-01-26 23:27:55 +0200

    w99

--- w99.js
+++ w99.js
@@ -1,22 +1,17 @@
-const url = "https://raw.githubusercontent.com/fredsmit/hello-world/master/t01.ts"
-
+"use strict";
+const url = "https://raw.githubusercontent.com/fredsmit/hello-world/master/t01.ts";
 const promise = fetch(url)
-  .then(response => response.json())
-  .catch(err => alert(err))
-
-  promise.then(async response => {
-
+    .then(response => response.json())
+    .catch(err => alert(err));
+promise.then(async (response) => {
     const dvHead = document.getElementById("dvHead");
     if (dvHead && dvHead.tagName === "H1") {
         dvHead.innerText = "Cool, the data is loaded.";
     }
-
     const div = document.createElement('code');
     const json = await response.json();
-
     div.innerText = JSON.stringify(json, null, 2);
     document.body.append(div);
 }).catch(error => {
     console.log(error);
 });
-
