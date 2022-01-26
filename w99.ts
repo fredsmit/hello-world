d44ca685adbd86e16bcfb8f8392fab072ac02dd3
ALFREDAS SMITA <ALFREDAS@lt.ibm.com>
2022-01-27 00:54:16 +0200

    thenable

--- w99.ts
+++ w99.ts
@@ -1,8 +1,36 @@
 const url = "https://raw.githubusercontent.com/fredsmit/hello-world/master/data.json";
 
-const promise = fetch(url);
+type TData = { name: string };
 
-promise.then(async response => {
+function getData(url: string): Promise<TData> {
+
+    url = (url ?? "").trim();
+    if (url.length === 0) {
+        return Promise.reject(Error("Invalid argument: url"));
+    }
+
+    return new Promise<TData>(async (resolve, reject) => {
+        try {
+            const response = await fetch(url);
+            if (response.status === 200) {
+                const json = await response.json();
+                if ("user" in json && json.user === "fredsmit") {
+                    resolve({ name: json.user });
+                } else {
+                    throw Error("Invalid data.");
+                }
+            } else {
+                const statusText = (response.statusText ?? "").trim();
+                const msg = statusText.length > 0 ? statusText : `HTTP response status: ${response.status}`;
+                reject(Error(msg));
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+getData(url).then((data: TData) => {
 
     const dvHead = document.getElementById("dvHead");
     if (dvHead && dvHead.tagName === "H1") {
@@ -10,14 +38,11 @@ promise.then(async response => {
     }
 
     const div = document.createElement('code');
-    const json = await response.json();
-
-    div.innerText = JSON.stringify(json, null, 2);
+    div.innerText = JSON.stringify(data, null, 2);
     document.body.append(div);
-    throw new Error("BUM");
-}, (reason: any) => {
-    console.log("reason:", reason);
-}).catch(error => {
-    console.log("ERROR:", error);
-});
 
+}, (reason: any) => {
+    console.log("Promise rejected. Reason:", reason);
+}).catch(reason => {
+    console.log("Promise processing failed. Reason:", reason);
+});
