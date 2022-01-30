4c780344f1c59f1e4ffc514b316fc0286cc32b05
ALFREDAS SMITA <ALFREDAS@lt.ibm.com>
2022-01-30 17:38:19 +0200

    a

---
+++ t06.js
@@ -0,0 +1,7 @@
+// string
+const div = document.getElementById("div");
+console.log(div.getAttribute('style')); // color:red;font-size:120%
+// object
+console.log(div.style); // [object CSSStyleDeclaration]
+console.log(div.style.color); // red
+export {};
