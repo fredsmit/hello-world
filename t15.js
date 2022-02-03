cb90fad493b4661e37da66474bffa0e74302e225
ALFREDAS SMITA <ALFREDAS@lt.ibm.com>
2022-02-03 16:15:05 +0200

    Inside/Outside

--- t15.js
+++ t15.js
@@ -6,7 +6,7 @@
 //         Math.round(rect.height)
 //     );
 // }
-function positionAt(anchor, note, position) {
+function positionAt(anchor, note, position, outside) {
     function getAnchorCoords(elem) {
         const rect = elem.getBoundingClientRect();
         // document.documentElement.scrollLeft/scrollTop
@@ -36,6 +36,7 @@ function positionAt(anchor, note, position) {
         case "left-top":
             // noteStyle.top = scrollTop + anchorTop + 2 + "px";
             // noteStyle.left = scrollLeft + anchorLeft + 2 + "px";
+            copyOfNote.dataset["pos"] = String(1);
             noteStyle.top = anchorTop + 2 + "px";
             noteStyle.left = anchorLeft + 2 + "px";
             break;
@@ -44,36 +45,61 @@ function positionAt(anchor, note, position) {
         case "right-top":
             // noteStyle.top = scrollTop + anchorTop + 2 + "px";
             // noteStyle.left = scrollLeft + anchorRight + 2 + "px";
+            copyOfNote.dataset["pos"] = String(2);
             noteStyle.top = anchorTop + 2 + "px";
             noteStyle.left = anchorRight + 2 + "px";
             break;
         case 3:
-        case "bottom-left":
-        case "left-bottom":
-            // noteStyle.top = scrollTop + anchorBottom + 2 + "px";
-            // noteStyle.left = scrollLeft + anchorLeft + 2 + "px";
-            noteStyle.top = anchorBottom + 2 + "px";
-            noteStyle.left = anchorLeft + 2 + "px";
-            break;
-        case 4:
         case "bottom-right":
         case "right-bottom":
             // noteStyle.top = scrollTop + anchorBottom + 2 + "px";
             // noteStyle.left = scrollLeft + anchorRight + 2 + "px";
+            copyOfNote.dataset["pos"] = String(3);
             noteStyle.top = anchorBottom + 2 + "px";
             noteStyle.left = anchorRight + 2 + "px";
+            break;
+        case 4:
+        case "bottom-left":
+        case "left-bottom":
+            // noteStyle.top = scrollTop + anchorBottom + 2 + "px";
+            // noteStyle.left = scrollLeft + anchorLeft + 2 + "px";
+            copyOfNote.dataset["pos"] = String(4);
+            noteStyle.top = anchorBottom + 2 + "px";
+            noteStyle.left = anchorLeft + 2 + "px";
             break;
     }
     if ((copyOfNote.textContent ?? "").trim().length === 0) {
         copyOfNote.textContent = "note\xA0@\xA0" + String(position);
     }
-    document.body.append(copyOfNote);
+    if (outside) {
+        document.body.append(copyOfNote);
+    }
+    else {
+        anchor.append(copyOfNote);
+        const noteRect = getAnchorCoords(copyOfNote);
+        const pos = copyOfNote.dataset["pos"];
+        switch (pos) {
+            case "1":
+                // OK
+                break;
+            case "2":
+                noteStyle.left = (noteRect.left - noteRect.width - 4) + "px";
+                break;
+            case "3":
+                noteStyle.top = (noteRect.top - noteRect.height - 4) + "px";
+                noteStyle.left = (noteRect.left - noteRect.width - 4) + "px";
+                break;
+            case "4":
+                noteStyle.top = (noteRect.top - noteRect.height - 4) + "px";
+                break;
+        }
+    }
 }
-function showNote(anchor, html, position) {
+function showNote(anchor, html, position, outside) {
     const note = document.createElement('div');
     note.className = "note";
     note.innerHTML = html;
-    positionAt(anchor, note, position);
+    positionAt(anchor, note, position, outside);
 }
 const blockquote = document.querySelector('blockquote');
 if (blockquote) {
@@ -87,28 +113,35 @@ if (blockquote) {
             }
         }
     });
+    function getShowNote(outside) {
+        return (anchor, html, position) => showNote(anchor, html, position, outside);
+    }
+    let outside = false;
+    const timeout = 2000;
     while (true) {
+        const showNote = getShowNote(outside);
+        outside = !outside;
         showNote(blockquote, "note @ top/left", "top-left");
         showNote(blockquote, "note @ top/right", "top-right");
         showNote(blockquote, "note @ bottom/left", "bottom-left");
         showNote(blockquote, "note @ bottom/right", "bottom-right");
-        await removeNotes(5000);
+        await removeNotes(timeout);
         showNote(blockquote, "", "top-left");
         showNote(blockquote, "", "top-right");
         showNote(blockquote, "", "bottom-left");
         showNote(blockquote, "", "bottom-right");
-        await removeNotes(5000);
+        await removeNotes(timeout);
         showNote(blockquote, "", 1);
         showNote(blockquote, "", 2);
         showNote(blockquote, "", 3);
         showNote(blockquote, "", 4);
-        await removeNotes(5000);
+        await removeNotes(timeout);
         showNote(blockquote, "");
-        await removeNotes(5000);
+        await removeNotes(timeout);
         showNote(blockquote, "", null);
-        await removeNotes(5000);
+        await removeNotes(timeout);
         showNote(blockquote, "", "kuku");
-        await removeNotes(5000);
+        await removeNotes(timeout);
     }
 }
 function removeNotes(timeout = 1000) {
--- t15.ts
+++ t15.ts
@@ -11,7 +11,10 @@ type PositionAt = "default" | 1 | 2 | 3 | 4
 //     );
 // }
 
-function positionAt(anchor: HTMLElement, note: HTMLElement, position?: PositionAt | null | undefined) {
+function positionAt(
+    anchor: HTMLElement, note: HTMLElement,
+    position?: PositionAt | null | undefined, outside?: boolean | null | undefined
+) {
 
     function getAnchorCoords(elem: HTMLElement): DOMRectReadOnly {
         const rect = elem.getBoundingClientRect();
@@ -59,6 +62,7 @@ function positionAt(anchor: HTMLElement, note: HTMLElement, position?: PositionA
         case "left-top":
             // noteStyle.top = scrollTop + anchorTop + 2 + "px";
             // noteStyle.left = scrollLeft + anchorLeft + 2 + "px";
+            copyOfNote.dataset["pos"] = String(1);
             noteStyle.top = anchorTop + 2 + "px";
             noteStyle.left = anchorLeft + 2 + "px";
             break;
@@ -67,24 +71,27 @@ function positionAt(anchor: HTMLElement, note: HTMLElement, position?: PositionA
         case "right-top":
             // noteStyle.top = scrollTop + anchorTop + 2 + "px";
             // noteStyle.left = scrollLeft + anchorRight + 2 + "px";
+            copyOfNote.dataset["pos"] = String(2);
             noteStyle.top = anchorTop + 2 + "px";
             noteStyle.left = anchorRight + 2 + "px";
             break;
         case 3:
-        case "bottom-left":
-        case "left-bottom":
-            // noteStyle.top = scrollTop + anchorBottom + 2 + "px";
-            // noteStyle.left = scrollLeft + anchorLeft + 2 + "px";
-            noteStyle.top = anchorBottom + 2 + "px";
-            noteStyle.left = anchorLeft + 2 + "px";
-            break;
-        case 4:
         case "bottom-right":
         case "right-bottom":
             // noteStyle.top = scrollTop + anchorBottom + 2 + "px";
             // noteStyle.left = scrollLeft + anchorRight + 2 + "px";
+            copyOfNote.dataset["pos"] = String(3);
             noteStyle.top = anchorBottom + 2 + "px";
             noteStyle.left = anchorRight + 2 + "px";
+            break;
+        case 4:
+        case "bottom-left":
+        case "left-bottom":
+            // noteStyle.top = scrollTop + anchorBottom + 2 + "px";
+            // noteStyle.left = scrollLeft + anchorLeft + 2 + "px";
+            copyOfNote.dataset["pos"] = String(4);
+            noteStyle.top = anchorBottom + 2 + "px";
+            noteStyle.left = anchorLeft + 2 + "px";
             break;
     }
 
@@ -92,14 +99,39 @@ function positionAt(anchor: HTMLElement, note: HTMLElement, position?: PositionA
         copyOfNote.textContent = "note\xA0@\xA0" + String(position);
     }
 
-    document.body.append(copyOfNote);
+    if (outside) {
+        document.body.append(copyOfNote);
+    } else {
+        anchor.append(copyOfNote);
+        const noteRect = getAnchorCoords(copyOfNote);
+        const pos = copyOfNote.dataset["pos"];
+        switch (pos) {
+            case "1":
+                // OK
+                break;
+            case "2":
+                noteStyle.left = (noteRect.left - noteRect.width - 4) + "px";
+                break;
+            case "3":
+                noteStyle.top = (noteRect.top - noteRect.height - 4) + "px";
+                noteStyle.left = (noteRect.left - noteRect.width - 4) + "px";
+                break;
+            case "4":
+                noteStyle.top = (noteRect.top - noteRect.height - 4) + "px";
+                break;
+        }
+
+    }
 }
 
-function showNote(anchor: HTMLElement, html: string, position?: PositionAt) {
+function showNote(
+    anchor: HTMLElement, html: string,
+    position?: PositionAt, outside?: boolean | null | undefined
+) {
     const note = document.createElement('div');
     note.className = "note";
     note.innerHTML = html;
-    positionAt(anchor, note, position);
+    positionAt(anchor, note, position, outside);
 }
 
 const blockquote: HTMLQuoteElement | null = document.querySelector('blockquote');
@@ -116,33 +148,45 @@ if (blockquote) {
         }
     })
 
+    function getShowNote(outside: boolean) {
+        return (
+            anchor: HTMLElement, html: string,
+            position?: PositionAt | undefined
+        ) => showNote(anchor, html, position, outside);
+    }
+
+    let outside = false;
+    const timeout = 2000;
     while (true) {
+        const showNote = getShowNote(outside);
+        outside = !outside;
+
         showNote(blockquote, "note @ top/left", "top-left");
         showNote(blockquote, "note @ top/right", "top-right");
         showNote(blockquote, "note @ bottom/left", "bottom-left");
         showNote(blockquote, "note @ bottom/right", "bottom-right");
-        await removeNotes(5000);
+        await removeNotes(timeout);
 
         showNote(blockquote, "", "top-left");
         showNote(blockquote, "", "top-right");
         showNote(blockquote, "", "bottom-left");
         showNote(blockquote, "", "bottom-right");
-        await removeNotes(5000);
+        await removeNotes(timeout);
 
         showNote(blockquote, "", 1);
         showNote(blockquote, "", 2);
         showNote(blockquote, "", 3);
         showNote(blockquote, "", 4);
-        await removeNotes(5000);
+        await removeNotes(timeout);
 
         showNote(blockquote, "");
-        await removeNotes(5000);
+        await removeNotes(timeout);
 
         showNote(blockquote, "", null as unknown as PositionAt);
-        await removeNotes(5000);
+        await removeNotes(timeout);
 
         showNote(blockquote, "", "kuku" as PositionAt);
-        await removeNotes(5000);
+        await removeNotes(timeout);
     }
 }
 
