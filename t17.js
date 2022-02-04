4fd93f0e2eca9e3bf4d52c990431aafe08ea9d6c
ALFREDAS SMITA <ALFREDAS@lt.ibm.com>
2022-02-04 15:34:20 +0200

    Mind the borders

--- t17.js
+++ t17.js
@@ -19,11 +19,13 @@ try {
     ball.style.top = top;
     ball.style.left = left;
     field.addEventListener("click", fieldEventListenerObject);
+    field.addEventListener("dblclick", fieldEventListenerObject);
 }
 catch (error) {
     console.error("Error:", error);
 }
 function getFieldEventListenerObject(field, ball) {
+    let extendedField = false;
     function getPageCoords(elem) {
         const rect = elem.getBoundingClientRect();
         return new DOMRectReadOnly(Math.round(rect.x + window.pageXOffset), Math.round(rect.y + window.pageYOffset), Math.round(rect.width), Math.round(rect.height));
@@ -31,28 +33,53 @@ function getFieldEventListenerObject(field, ball) {
     function getTranslatedCoords(elemRect, anchorRect) {
         return new DOMRectReadOnly(elemRect.x - anchorRect.x, elemRect.y - anchorRect.y, elemRect.width, elemRect.height);
     }
+    function onDblClick(ev) {
+        extendedField = !extendedField;
+        const fieldBorderColor = extendedField ? "darkgreen" : "";
+        field.style.borderColor = fieldBorderColor;
+        field.style.color = fieldBorderColor;
+    }
+    function onClick(ev) {
+        const pageOffsetX = Math.round(window.pageXOffset);
+        const pageOffsetY = Math.round(window.pageYOffset);
+        const anchorRect = getPageCoords(field);
+        const fieldRect = getTranslatedCoords(anchorRect, anchorRect);
+        const ballRect = getTranslatedCoords(getPageCoords(ball), anchorRect);
+        const borderTop = field.clientTop;
+        const borderRight = fieldRect.width - field.clientLeft - field.clientWidth;
+        const borderBottom = fieldRect.height - field.clientTop - field.clientHeight;
+        const borderLeft = field.clientLeft;
+        // console.log("borderTop:", borderTop);
+        // console.log("borderRight:", borderRight);
+        // console.log("borderBottom:", borderBottom);
+        // console.log("borderLeft:", borderLeft);
+        const pointX = ev.clientX - anchorRect.x - borderLeft + pageOffsetX;
+        const pointY = ev.clientY - anchorRect.y - borderTop + pageOffsetY;
+        // console.log("pointX, pointY:", pointX, pointY);
+        const ballLeft = Math.round(pointX - ballRect.width / 2);
+        const ballTop = Math.round(pointY - ballRect.height / 2);
+        // console.log("ballLeft, ballTop:", ballLeft, ballTop);
+        const ballLeftMin = extendedField ? -borderLeft : 0;
+        const ballLeftMax = Math.round(field.clientWidth - ballRect.width + (extendedField ? borderRight : 0));
+        const ballTopMin = extendedField ? -borderTop : 0;
+        const ballTopMax = Math.round(field.clientHeight - ballRect.height + (extendedField ? borderBottom : 0));
+        ball.style.left = Math.max(ballLeftMin, Math.min(ballLeft, ballLeftMax)) + "px";
+        ball.style.top = Math.max(ballTopMin, Math.min(ballTop, ballTopMax)) + "px";
+    }
     const fieldClickObject = {
         handleEvent: function (ev) {
-            if (ev.type === "click" && ev instanceof PointerEvent) {
-                const pageOffsetX = Math.round(window.pageXOffset);
-                const pageOffsetY = Math.round(window.pageYOffset);
-                const anchorRect = getPageCoords(field);
-                const fieldRect = getTranslatedCoords(anchorRect, anchorRect);
-                const ballRect = getTranslatedCoords(getPageCoords(ball), anchorRect);
-                const horizontalBorderThickness = Math.round((fieldRect.height - field.clientHeight) / 2);
-                const verticalBorderThickness = Math.round((fieldRect.width - field.clientWidth) / 2);
-                const pointX = ev.clientX - anchorRect.x - verticalBorderThickness + pageOffsetX;
-                const pointY = ev.clientY - anchorRect.y - horizontalBorderThickness + pageOffsetY;
-                const ballLeft = Math.round(pointX - ballRect.width / 2);
-                const ballTop = Math.round(pointY - ballRect.height / 2);
-                const ballLeftMin = 0;
-                const ballLeftMax = Math.round(field.clientWidth - ballRect.width);
-                const ballTopMin = 0;
-                const ballTopMax = Math.round(field.clientHeight - ballRect.height);
-                ball.style.left = Math.max(ballLeftMin, Math.min(ballLeft, ballLeftMax)) + "px";
-                ball.style.top = Math.max(ballTopMin, Math.min(ballTop, ballTopMax)) + "px";
+            if (ev instanceof MouseEvent) {
+                switch (ev.type) {
+                    case "click":
+                        onClick(ev);
+                        break;
+                    case "dblclick":
+                        onDblClick(ev);
+                        break;
+                }
             }
         }
     };
     return fieldClickObject;
 }
+;
--- t17.ts
+++ t17.ts
@@ -30,12 +30,15 @@ try {
     ball.style.left = left;
 
     field.addEventListener("click", fieldEventListenerObject);
+    field.addEventListener("dblclick", fieldEventListenerObject);
 
 } catch (error) {
     console.error("Error:", error);
 }
 
 function getFieldEventListenerObject(field: HTMLElement, ball: HTMLElement): EventListenerObject {
+
+    let extendedField: boolean = false;
 
     function getPageCoords(elem: HTMLElement): DOMRectReadOnly {
         const rect = elem.getBoundingClientRect();
@@ -56,41 +59,66 @@ function getFieldEventListenerObject(field: HTMLElement, ball: HTMLElement): Eve
         );
     }
 
+    function onDblClick(ev: MouseEvent) {
+        extendedField = !extendedField;
+        const fieldBorderColor = extendedField ? "darkgreen" : "";
+        field.style.borderColor = fieldBorderColor;
+        field.style.color = fieldBorderColor;
+    }
+
+    function onClick(ev: MouseEvent) {
+        const pageOffsetX = Math.round(window.pageXOffset);
+        const pageOffsetY = Math.round(window.pageYOffset);
+
+        const anchorRect: DOMRectReadOnly = getPageCoords(field);
+
+        const fieldRect: DOMRectReadOnly = getTranslatedCoords(anchorRect, anchorRect);
+        const ballRect = getTranslatedCoords(getPageCoords(ball), anchorRect);
+
+        const borderTop = field.clientTop;
+        const borderRight = fieldRect.width - field.clientLeft - field.clientWidth;
+        const borderBottom = fieldRect.height - field.clientTop - field.clientHeight;
+        const borderLeft = field.clientLeft;
+
+        // console.log("borderTop:", borderTop);
+        // console.log("borderRight:", borderRight);
+        // console.log("borderBottom:", borderBottom);
+        // console.log("borderLeft:", borderLeft);
+        const pointX = ev.clientX - anchorRect.x - borderLeft + pageOffsetX;
+        const pointY = ev.clientY - anchorRect.y - borderTop + pageOffsetY;
+        // console.log("pointX, pointY:", pointX, pointY);
+        const ballLeft = Math.round(pointX - ballRect.width / 2);
+        const ballTop = Math.round(pointY - ballRect.height / 2);
+        // console.log("ballLeft, ballTop:", ballLeft, ballTop);
+        const ballLeftMin = extendedField ? -borderLeft : 0;
+        const ballLeftMax = Math.round(
+            field.clientWidth - ballRect.width + (extendedField ? borderRight : 0));
+
+        const ballTopMin = extendedField ? -borderTop : 0;
+        const ballTopMax = Math.round(
+            field.clientHeight - ballRect.height + (extendedField ? borderBottom : 0));
+
+        ball.style.left = Math.max(ballLeftMin, Math.min(ballLeft, ballLeftMax)) + "px";
+        ball.style.top = Math.max(ballTopMin, Math.min(ballTop, ballTopMax)) + "px";
+    }
+
     const fieldClickObject: EventListenerObject = {
         handleEvent: function (this: any, ev: Event): void {
-            if (ev.type === "click" && ev instanceof PointerEvent) {
-
-                const pageOffsetX = Math.round(window.pageXOffset);
-                const pageOffsetY = Math.round(window.pageYOffset);
-
-                const anchorRect: DOMRectReadOnly = getPageCoords(field);
-
-                const fieldRect: DOMRectReadOnly = getTranslatedCoords(anchorRect, anchorRect);
-                const ballRect = getTranslatedCoords(getPageCoords(ball), anchorRect);
-                const horizontalBorderThickness = Math.round((fieldRect.height - field.clientHeight) / 2);
-                const verticalBorderThickness = Math.round((fieldRect.width - field.clientWidth) / 2);
-
-                const pointX = ev.clientX - anchorRect.x - verticalBorderThickness + pageOffsetX;
-                const pointY = ev.clientY - anchorRect.y - horizontalBorderThickness + pageOffsetY;
-
-                const ballLeft = Math.round(pointX - ballRect.width / 2);
-                const ballTop = Math.round(pointY - ballRect.height / 2);
-
-                const ballLeftMin = 0;
-                const ballLeftMax = Math.round(field.clientWidth - ballRect.width);
-
-                const ballTopMin = 0;
-                const ballTopMax = Math.round(field.clientHeight - ballRect.height);
-
-                ball.style.left = Math.max(ballLeftMin, Math.min(ballLeft, ballLeftMax)) + "px";
-                ball.style.top = Math.max(ballTopMin, Math.min(ballTop, ballTopMax)) + "px";
+            if (ev instanceof MouseEvent) {
+                switch (ev.type) {
+                    case "click":
+                        onClick(ev);
+                        break;
+                    case "dblclick":
+                        onDblClick(ev);
+                        break;
+                }
             }
         }
-    };
+    }
 
     return fieldClickObject;
-}
-
+};
 
 export { };
 
