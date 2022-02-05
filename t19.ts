81a95632079d1b807fc9166aa499406614aae0f5
ALFREDAS SMITA <ALFREDAS@lt.ibm.com>
2022-02-05 10:02:43 +0200

    remove cats

---
+++ t19.ts
@@ -0,0 +1,31 @@
+const removeButton = getRemoveButton();
+if (removeButton === null)
+    throw Error("Required 'remove-button' not found.");
+
+const paneHeaderCollection = document.body.querySelectorAll("div.pane h3");
+for (const paneHeader of paneHeaderCollection) {
+    const removeButtonCopy = removeButton.cloneNode(true);
+    removeButtonCopy.addEventListener("click", clickHandler);
+    paneHeader.append(removeButtonCopy);
+}
+
+function clickHandler(ev: Event): void {
+    for (const el of ev.composedPath()) {
+        if (el instanceof Element && el.classList.contains("pane")) {
+            el.remove();
+            break;
+        }
+    }
+};
+
+function getRemoveButton(): Element | null {
+    const removeButtonCollection = document.body.getElementsByClassName("remove-button");
+    if (removeButtonCollection.length > 0) {
+        const removeButton = removeButtonCollection[0];
+        removeButton.remove();
+        return removeButton;
+    }
+    return null;
+}
+
+export { };
