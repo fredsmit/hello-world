dcd919505c8ce229240acbe5dd2cdb31ea0f970a
ALFREDAS SMITA <ALFREDAS@lt.ibm.com>
2022-02-16 12:54:56 +0200

    chatClient

--- t62.ts
+++ t62.ts
@@ -108,3 +108,25 @@ socket.onerror = function (this: WebSocket, ev: Event) {
 };
 
 export { };
+
+// 1000	Normal Closure
+// 1001	Going Away
+// 1002	Protocol error
+// 1003	Unsupported Data
+// 1004	Reserved
+// 1005	No Status Rcvd
+// 1006	Abnormal Closure
+// 1007	Invalid frame payload data
+// 1008	Policy Violation
+// 1009	Message Too Big
+// 1010	Mandatory Ext.
+// 1011	Internal Error
+// 1012	Service Restart
+// 1013	Try Again Later
+// 1014	The server was acting as a gateway or proxy and received an invalid response from the upstream server.
+//      This is similar to 502 HTTP Status Code.
+// 1015	TLS handshake
+// 1016-2999
+// 3000	Unauthorized
+// 3001-3999 Unassigned
+// 4000-4999 Reserved for Private Use
